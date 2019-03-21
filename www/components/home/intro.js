2bd9e13b0a83842ad194cd2f2cddda19f3757f1e
JJ Kasper <jj@jjsweb.site>
2019-03-21 17:01:36 -0500

    Make intro logo AMP compatible

--- www/components/home/intro.js
+++ www/components/home/intro.js
@@ -1,4 +1,3 @@
-/* global window */
 import React from 'react';
 import classNames from 'classnames';
 import Link from 'next/link';
@@ -6,94 +5,85 @@ import Link from 'next/link';
 import Logo from '../logo';
 import Container from '../container';
 import Button from '../button';
-import Popover from '../popover';
 import Campaign from './campaign';
 
 import { links } from '../../site-manifest';
 
-function easing(t) {
-  // eslint-disable-next-line
-  return 1 + --t * t * t * t * t;
-}
+export function LogoContainer () {
+  return (
+    <div style={{
+        position: 'sticky',
+        top: -5,
+        left: 0,
+        right: 0,
+        zIndex: 1000
+      }}>
+     
+      <amp-animation id='logoAnim' layout='nodisplay'>
+        {/* eslint-disable-next-line */}
+        <script type="application/json" dangerouslySetInnerHTML={{
+          __html: `
+            {
+              "fill": "both",
+              "duration": "1",
+              "direction": "alternate",
+              "animations": [
+                {
+                  "selector": ".logo-main",
+                  "keyframes": [
+                    { "transform": "scale(1)" },
+                    { "transform": "scale(0.8)" }
+                  ]
+                }
+              ]
+            }
+          `
+        }} />
+      </amp-animation>
+      <amp-animation id='logoVAnim' layout='nodisplay'>
+        {/* eslint-disable-next-line */}
+        <script type="application/json" dangerouslySetInnerHTML={{
+          __html: `
+            {
+              "fill": "both",
+              "duration": "1",
+              "delay": "50ms",
+              "direction": "alternate",
+              "animations": [
+                {
+                  "selector": ".logo-main .version",
+                  "keyframes": [
+                    { "opacity": 1 },
+                    { "opacity": 0 }
+                  ]
+                }
+              ]
+            }
+          `
+        }} />
+      </amp-animation>
+      <amp-position-observer
+        layout="nodisplay"
+        intersection-ratios="1"
+        on="scroll:logoAnim.seekTo(percent=event.percent),logoVAnim.seekTo(percent=event.percent)"
+      />
 
-class LogoContainer extends React.PureComponent {
-  state = {
-    scroll: 0
-  };
-
-  componentDidMount() {
-    this.setState({ mounted: true });
-    window.addEventListener('scroll', this.onScroll);
-    this.onScroll();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll);
-  }
-
-  onScroll = () => {
-    const scroll = window.scrollY || window.document.body.scrollTop;
-    this.setState({ scroll });
-  };
-
-  render() {
-    const { scroll, mounted } = this.state;
-
-    // const LOGO_TOP = isMobile ? 126 : 170;
-    const LOGO_TOP = 170;
-    return (
-      <div
-        className={classNames('logo-main f4 fw6', { unmounted: !mounted })}
-        style={{
-          top: Math.max(LOGO_TOP - scroll, 7),
-          willChange: `transform`,
-          transform: `scale(${Math.max(easing(1 - scroll / LOGO_TOP), 0) *
-            0.325 +
-            0.625}) translate3d(0, 0, 0)`,
-          transformOrigin: 'top'
-        }}
-      >
+      <div className='logo-main f4 fw6'>
         <Link href="/">
-          <a
-            className={scroll >= LOGO_TOP ? null : 'disable'}
-            aria-label="Next.js"
-          >
+          <a aria-label="Next.js">
             <Logo size={80} />
           </a>
         </Link>
         <Link href="/blog/next-8">
-          <a
-            className="version no-tap-highlight no-drag"
-            style={{
-              opacity: Math.max(1 - (scroll * 3) / LOGO_TOP, 0),
-              visibility: scroll * 3 > LOGO_TOP ? 'hidden' : 'visible'
-            }}
-          >
-            <Popover
-              content={
-                <span className="f5 fw4 tip">
-                  What’s new in <strong className="fw7">8</strong>?
-                </span>
-              }
-              top={65}
-            >
-              8
-            </Popover>
-          </a>
+          <a className="version no-tap-highlight no-drag">8</a>
         </Link>
         <style jsx>{`
-          .disable {
-            pointer-events: none;
-          }
           .logo-main {
             display: flex;
-            position: fixed;
             justify-content: center;
             color: #0076ff;
-            left: 0;
-            right: 0;
             width: 200px;
-            margin: auto;
+            margin: 140px auto -210px;
             z-index: 1000;
           }
           .logo-main .version {
@@ -102,18 +92,8 @@ class LogoContainer extends React.PureComponent {
             margin-top: 0.4rem;
             cursor: pointer;
           }
-          .version.hide {
-            opacity: 0;
-          }
           a.version {
             color: #0076ff;
-          }
-          .version .tip {
-            color: #111;
-            white-space: nowrap;
-          }
-          .unmounted {
-            display: none;
           }
           // CSS only media query for mobile
           @media screen and (max-width: 640px) {
@@ -123,130 +103,117 @@ class LogoContainer extends React.PureComponent {
           }
         `}</style>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default class extends React.PureComponent {
-  render() {
-    return (
-      <Container
-        role="main"
-        wide
-        center
-        overflow
-        dotBackground
-        minHeight={564}
-        mobileStyle={'min-height: 460px;'}
-        style={{
-          display: 'flex',
-          alignItems: 'flex-end'
-        }}
-      >
-        <Container>
-          <div className="intro-container">
-            <style jsx>{`
-              .intro-container {
-                margin: 0 0 2rem 0;
-                overflow: visible;
-              }
-              h2 {
-                margin-top: 1rem;
-                margin-bottom: 2rem;
-              }
-              .main-button {
-                margin-bottom: 2rem;
-              }
-              .links {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              }
-              .links > * {
-                padding: 0 0.5rem;
-              }
-              .campaign {
-                width: 100%;
-                letter-spacing: -0.02rem;
-                overflow: hidden;
-                pointer-events: none;
-                cursor: default;
-                z-index: -1;
-              }
-              .f-xs-0 {
-                font-size: 2.887rem; /* 2.566rem; /* 2.281rem; */
-              }
-              .f-xs-1 {
-                font-size: 2.027rem; //.566rem;
-              }
-              .logo-main {
-                position: fixed;
-                display: flex;
-                justify-content: center;
-                color: #0076ff;
-                left: 0;
-                right: 0;
-                width: 200px;
-                margin: auto;
-                z-index: 1000;
-              }
+export default function Intro() {
+  return (
+    <Container
+      role="main"
+      wide
+      center
+      overflow
+      dotBackground
+      minHeight={564}
+      mobileStyle={'min-height: 460px;'}
+      style={{
+        display: 'flex',
+        alignItems: 'flex-end'
+      }}
+    >
+      <Container>
+        <div className="intro-container">
+          <style jsx>{`
+            .intro-container {
+              margin: 0 0 2rem 0;
+              overflow: visible;
+            }
+            h2 {
+              margin-top: 1rem;
+              margin-bottom: 2rem;
+            }
+            .main-button {
+              margin-bottom: 2rem;
+            }
+            .links {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+            .links > * {
+              padding: 0 0.5rem;
+            }
+            .campaign {
+              width: 100%;
+              letter-spacing: -0.02rem;
+              overflow: hidden;
+              pointer-events: none;
+              cursor: default;
+              z-index: -1;
+            }
+            .f-xs-0 {
+              font-size: 2.887rem; /* 2.566rem; /* 2.281rem; */
+            }
+            .f-xs-1 {
+              font-size: 2.027rem; //.566rem;
+            }
+            .title-1 {
+              font-size: 1.802032470703125em;
+            }
+            .title-2 {
+              font-size: 2.887rem;
+              margin-top: -4.2rem;
+              margin-bottom: 0;
+            }
+            .campaign {
+              margin: 1rem 0 1.6rem;
+            }
+            // CSS only media query for mobile
+            @media screen and (max-width: 640px) {
               .title-1 {
-                font-size: 1.802032470703125em;
+                font-size: 1.423828125em;
               }
               .title-2 {
-                font-size: 2.887rem;
-                margin-top: -4.2rem;
-                margin-bottom: 0;
+                font-size: 1.802032470703125em;
+                margin-top: -2.4rem;
               }
               .campaign {
-                margin: 1rem 0 1.6rem;
+                margin: 0 0 2rem;
               }
-              // CSS only media query for mobile
-              @media screen and (max-width: 640px) {
-                .title-1 {
-                  font-size: 1.423828125em;
-                }
-                .title-2 {
-                  font-size: 1.802032470703125em;
-                  margin-top: -2.4rem;
-                }
-                .campaign {
-                  margin: 0 0 2rem;
-                }
-              }
-            `}</style>
-            <LogoContainer />
-            <div className="campaign no-drag no-tap-highlight">
-              <h1 className={classNames('title-1', 'fw6')}>
-                The React Framework for
-              </h1>
-              <h2 className={classNames('title-2', 'fw7')}>
-                <Campaign />
-              </h2>
+            }
+          `}</style>
+          {/* <LogoContainer /> */}
+          <div className="campaign no-drag no-tap-highlight">
+            <h1 className={classNames('title-1', 'fw6')}>
+              The React Framework for
+            </h1>
+            <h2 className={classNames('title-2', 'fw7')}>
+              <Campaign />
+            </h2>
+          </div>
+          <div>
+            <div className="main-button">
+              <Button href="#showcases" invert>
+                See Showcase
+              </Button>
             </div>
-            <div>
-              <div className="main-button">
-                <Button href="#showcases" invert>
-                  See Showcase
-                </Button>
+            <div className="links">
+              <Link href={links.license}>
+                <a rel="noreferrer" target="_blank">
+                  <span className="mute">License: MIT</span>
+                </a>
+              </Link>
+              <div>
+                <Button href="/docs">View Docs</Button>
               </div>
-              <div className="links">
-                <Link href={links.license}>
-                  <a rel="noreferrer" target="_blank">
-                    <span className="mute">License: MIT</span>
-                  </a>
-                </Link>
-                <div>
-                  <Button href="/docs">View Docs</Button>
-                </div>
-                <div>
-                  <Button href="https://github.com/zeit/next.js">GitHub</Button>
-                </div>
+              <div>
+                <Button href="https://github.com/zeit/next.js">GitHub</Button>
               </div>
             </div>
           </div>
-        </Container>
+        </div>
       </Container>
-    );
-  }
+    </Container>
+  );
 }
--- www/pages/index.js
+++ www/pages/index.js
@@ -1,4 +1,6 @@
 import { SkipNavContent } from '@reach/skip-nav';
+import { useAmp } from 'next/amp';
+import Head from 'next/head'
 
 import Page from '../components/page';
 import Header from '../components/header';
@@ -6,40 +8,49 @@ import Footer from '../components/footer';
 import Navbar from '../components/navbar';
 import Notification from '../components/notification';
 
-import Intro from '../components/home/intro';
+import Intro, { LogoContainer } from '../components/home/intro';
 import Demo from '../components/home/demo';
 import Features from '../components/home/features';
 import Customers from '../components/home/customers';
 import Learn from '../components/home/learn';
 import SocialMeta from '../components/social-meta';
 
-export default () => (
-  <>
-    <Header height={32} shadow>
-      <Notification
-        href="/blog/next-8"
-        title="Next 8 is out!"
-        titleMobile="Next 8 is out!"
-      >
-        Next 8 is out! — Serverless mode, performance and security improvements
-        and more. Monday, February 11th 2019
-      </Notification>
-      <Navbar hideLogo={{ desktop: true, mobile: false }} />
-    </Header>
-    <Page title="Next.js - The React Framework">
-      <SocialMeta
-        image="/static/twitter-cards/home.jpg"
-        title="Next.js - The React Framework"
-        url="https://nextjs.org"
-        description="Production grade React applications that scale. The world’s leading companies use Next.js to build server-rendered applications, static websites, and more."
-      />
-      <SkipNavContent />
-      <Intro />
-      <Demo />
-      <Features />
-      <Customers />
-      <Learn />
-      <Footer />
-    </Page>
-  </>
-);
+export default () => {
+  const isAmp = useAmp();
+
+  return (
+    <>
+      <Head>
+        <script async custom-element="amp-position-observer" src="https://cdn.ampproject.org/v0/amp-position-observer-0.1.js"/>
+        <script async custom-element="amp-animation" src="https://cdn.ampproject.org/v0/amp-animation-0.1.js"/>
+      </Head>
+      <Header height={32} shadow>
+        <Notification
+          href="/blog/next-8"
+          title="Next 8 is out!"
+          titleMobile="Next 8 is out!"
+        >
+          Next 8 is out! — Serverless mode, performance and security
+          improvements and more. Monday, February 11th 2019
+        </Notification>
+        <Navbar hideLogo={{ desktop: true, mobile: false }} />
+      </Header>
+      <LogoContainer />
+      <Page title="Next.js - The React Framework">
+        <SocialMeta
+          image="/static/twitter-cards/home.jpg"
+          title="Next.js - The React Framework"
+          url="https://nextjs.org"
+          description="Production grade React applications that scale. The world’s leading companies use Next.js to build server-rendered applications, static websites, and more."
+        />
+        <SkipNavContent />
+        <Intro />
+        <Demo />
+        <Features />
+        <Customers />
+        <Learn />
+        <Footer />
+      </Page>
+    </>
+  );
+};
